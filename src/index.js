9cc63a0f9782974de5b635623308d68b7f197337
sajad126502 <77586522+sajad126502@users.noreply.github.com>
2021-05-14 21:58:31 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 express=require("express")
 app=express()
+web_port=process.env.PORT || 4000
 app.get("/",(req,res)=>{
+    
 res.send("<h1>hello my name is sajad  bashir naikoo</h1>")
 })
-app.listen(4000,()=>{
+app.listen(web_port,()=>{
     console.log("app is running on port 4000")
 })
